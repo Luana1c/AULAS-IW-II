6dcb63187c1ad0a3d7988ad07a1e9aeda8cf938d
Luana1c <luanagm09@gmail.com>
2024-02-29 11:07:41 -0300

    base

---
+++ AULA-03/js/executa.js
@@ -0,0 +1,33 @@
+const numero1= document.getElementById('numero1')
+const numero2= document.getElementById('numero2')
+const texto = document.getElementById('texto')
+const resultado = document.getElementById('resultado')
+
+document.writeln (numero1 + numero2) 
+
+
+function somar(){
+    // var numero1 document.writeln (numero1 + numero2) 
+    /* resultado.innerHTML = numero1.value + numero2.value */
+}
+function subtrair(){
+    /* var document.writeln (numero1 + numero2)  */
+    
+}
+function multiplicar(){
+    /* var document.writeln (numero1 + numero2)  */
+    
+}
+function dividir(){
+    /* var document.writeln (numero1 + numero2)  */
+}
+
+
+
+
+
+// function pegaNome(){
+//     var nome = document.getElementById ('nome').value
+//     // console.log('nome:',nome)
+//     resultado.innerHTML = nome
+// }
