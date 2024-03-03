a368e9f8f34911cb92c9f52fcd870d41f444b32b
Luana1c <luanagm09@gmail.com>
2024-03-03 17:57:02 -0300

    css e js prontos

--- AULA-03/js/executa.js
+++ AULA-03/js/executa.js
@@ -1,33 +1,57 @@
-const numero1= document.getElementById('numero1')
-const numero2= document.getElementById('numero2')
-const texto = document.getElementById('texto')
-const resultado = document.getElementById('resultado')
-
-document.writeln (numero1 + numero2) 
-
-
-function somar(){
-    // var numero1 document.writeln (numero1 + numero2) 
-    /* resultado.innerHTML = numero1.value + numero2.value */
-}
-function subtrair(){
-    /* var document.writeln (numero1 + numero2)  */
+function insert(num){
+     var numero = document.getElementById('resultado').innerHTML;
+     document.getElementById('resultado').innerHTML = numero + num;
     
 }
-function multiplicar(){
-    /* var document.writeln (numero1 + numero2)  */
-    
+function clean(){
+     document.getElementById('resultado').innerHTML = "";
 }
-function dividir(){
-    /* var document.writeln (numero1 + numero2)  */
+function back(){
+     var resultado = document.getElementById('resultado').innerHTML;
+     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else{
+        document.getElementById('resultado').innerHTML = "Nada..."
+    }
 }
 
 
 
 
 
-// function pegaNome(){
-//     var nome = document.getElementById ('nome').value
-//     // console.log('nome:',nome)
-//     resultado.innerHTML = nome
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
