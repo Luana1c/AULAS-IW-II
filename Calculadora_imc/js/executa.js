1e4c23b7eeae379ba094c5a4405b5b89d4d0687d
Luana1c <luanagm09@gmail.com>
2024-03-11 21:10:53 -0300

    finalizada

--- Calculadora_imc/js/executa.js
+++ Calculadora_imc/js/executa.js
@@ -5,21 +5,24 @@ function calcular(){
     var imc = peso / altura **2;
     var texto=""
    
-    if (imc<18){
-         texto="abaixo do ideal"
+    if (imc<18.5){
+         texto="magreza"
     }
-    else if(imc<24){
+    else if(imc<24.9){
          texto="Ideal"
     }
-    else if(imc<29){
+    else if(imc<29.9){
          texto="sobrepeso" 
     }
+    else if(imc<34.9){
+         texto="obesidade grau I" 
+    }
     else if(imc<39.9){
-         texto="obesidade" 
+         texto="obesidade grau II"
     }
-    else if(imc>39.9){
-         texto="obesidade extrema"
-    }
+    else if(imc>40){
+     texto="obesidade grau III"
+}
 
 
 document.getElementById("texto").innerHTML=texto
