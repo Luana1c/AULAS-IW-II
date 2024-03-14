5248ed51fd1ad8f05ee424ea61b4dfc83e90f37c
Luana1c <luanagm09@gmail.com>
2024-03-14 09:04:42 -0300

    exercicios do 14/3

---
+++ ATT5/js/executa.js
@@ -0,0 +1,7 @@
+var nome = prompt('Digite seu nome: ')
+        console.log('O nome é: ' , nome)    /* '+' ou ','' serve para a mesma coisa; preferencia ao '+' */
+        console.log(typeof(nome))
+        alert('Bem vindo '+ nome) 
+
+
+        // colocar no prompt o nome do produto quandoapertar o botão, depois colocar o valor e no final informar em um paragrafo
