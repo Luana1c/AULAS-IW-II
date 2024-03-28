227eab5835e32a7600fb30e80ce55dc7df58ff5d
Luana1c <luanagm09@.com>
2024-03-28 09:06:25 -0300

    deois eu vejo

---
+++ manipulando/js/executa.js
@@ -0,0 +1,60 @@
+//let titulo = document.querySelector('h1')
+//titulo.textContent = 'Aula 09 Manipular CSS'
+titulo.innerHTML = 'Aula Manipular CSS'
+//let imagem = document.querySelector('#foto')
+//imagem.setAttribute('src', 'img/pride-fc.jpg')
+//imagem.setAttribute('width', '280px')
+
+/* MANIPULAR CSS */
+//document.querySelector('h1').style.color = "red";
+//titulo.style.color = "red";
+//titulo.style.backgroundColor = "#000";
+//titulo.style.borderBottom = "2px solid red";
+//titulo.style.padding = "0.625rem";
+//titulo.style.borderRadius = "5px";
+
+//let box = document.querySelectorAll('.box')
+//box[0].setAttribute('class', 'escura')
+//box[0].removeAttribute('class')
+
+/////// MODOS DE COR ///////
+let tela = document.querySelector('main')
+
+let btnDark = document.querySelector('#btdark')
+let btnLight = document.querySelector('#btlight')
+let btnPink = document.querySelector('#btpink')
+
+btnDark.addEventListener('click', modoDark)
+btnLight.addEventListener('click', modoLight)
+btnPink.addEventListener('click', modoPink)
+
+
+function modoDark() {
+    //event.preventDefault();
+    console.log('modo dark')
+    tela.classList.add("dark");
+    tela.classList.remove("light");
+}
+
+function modoLight() {
+    //event.preventDefault();
+    console.log('modo light')
+    tela.classList.remove("dark");
+    tela.classList.add("light");
+}
+function modoPink() {
+    //event.preventDefault();
+    console.log('modo pink')
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.add("pink");
+}
+
+
+/*
+titulo.addEventListener('click', mudarId)
+function mudarId() {
+    titulo.id = 'titulo'
+    console.log(titulo.id)
+}
+*/
